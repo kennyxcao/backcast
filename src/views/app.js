0ff72d25f3172af41982e91b30c0120573d1227b
Hack Reactor Student <student@hackreactor.com>
2017-09-07 18:19:34 -0700

    Refactor

--- src/views/app.js
+++ src/views/app.js
@@ -3,12 +3,12 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.$el.html(this.template());
     this.videos = new Videos();
     this.render();
   },
 
   render: function() {
+    this.$el.html(this.template());
     new VideoListView({el: this.$('.list'), collection: this.videos}).render();
     new VideoPlayerView({el: this.$('.player'), collection: this.videos}).render();
     new SearchView({el: this.$('.search'), collection: this.videos}).render();
